637c05e2d7d6586ebdc33435f242445d16fe2e27
Jim00000 <good0121good@gmail.com>
2017-01-04 15:52:25 +0800

    add 'category' column

--- public/javascripts/idtable.js
+++ public/javascripts/idtable.js
@@ -1,11 +1,16 @@
 $(document).ready(function(){
   $('#example').DataTable( {
         "ajax": 'tables/idtable.ajax',
+		'aoColumns': [
+		    { sWidth: "20%", bSearchable: true, bSortable: true },
+            { sWidth: "40%", bSearchable: true, bSortable: false },
+            { sWidth: "30%", bSearchable: true, bSortable: true }
+		],
         "bSort":            false,
         "scrollY":          "240px",
         "scrollCollapse":   false,
         "info":             true,
-        "ordering":         false,
+        "ordering":         true,
         "paging":           true,
         "searching":        true
     } );
